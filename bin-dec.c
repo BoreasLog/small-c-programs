c2df8cec6e5410c9be79674c07383173fd06b25b
BoreasLog <clementfranklin99@gmail.com>
2021-06-01 02:26:17 -0400

    its converts it in reverse so thats intresting

--- bin-dec.c
+++ bin-dec.c
@@ -1,9 +1,23 @@
-# include <stdio.h>
+#include <stdio.h>
 
 
 int main(){
 	
-	printf("hello world!");
+	int input, i, j;
+	int output;
+
+	printf("Enter a Dec Value to convert to bin: ");
+	scanf("%d", &input);
+	
+	while(input !=0){
+		if(input % 2 == 0){
+			printf("0");
+		}else{
+			printf("1");
+		}	
+		input = input/2;
+	
+	}
 
 	return 0;
 }
